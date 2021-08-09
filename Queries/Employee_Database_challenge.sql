6fb82973c1d95fe93403ee205cb2fc0edb060a5b
dalejandri <86029441+dalejandri@users.noreply.github.com>
2021-08-08 20:44:27 -0500

    Deliverable 2
    The Employees Eligible for the Mentorship Program

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
 -- Create retirement_titles table
 SELECT e.emp_no,
 	e.first_name,
@@ -28,16 +30,21 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
 
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
 
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-    e.first_name,
-    e.last_name,
-    t.title,
-    t.from_date,
-    t.to_date
--- INTO new_retirement_titles
-FROM employees as e
-INNER JOIN titles as t
-on (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no, t.from_date DESC;
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date, 
+	de.from_date, 
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT JOIN dept_employee AS de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
